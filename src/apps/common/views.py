8dfe5b1d84c8f13e098b9c84259adb13365dbc5b
Macktireh <abdimack97@gmail.com>
2024-07-11 18:31:35 +0200

    code refactoring

--- src/apps/common/views.py
+++ src/apps/common/views.py
@@ -2,5 +2,5 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
-def error_404_view(request: HttpRequest, exception) -> HttpResponse:
+def error_404_view(request: HttpRequest, exception: Exception) -> HttpResponse:
     return render(request=request, template_name='errors/404.html')
