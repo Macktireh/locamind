eece7e769ae348381409add80947ca08e29b1ef1
Macktireh <abdimack97@gmail.com>
2024-09-07 18:47:23 +0200

    rename App BienRental to Locamind

--- src/apps/common/context_processors.py
+++ src/apps/common/context_processors.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 def common_processor(request) -> dict[str, Any]:
     return {
-        "APP_NAME": "BienRental",
+        "APP_NAME": "Locamind",
         "APP_DOMAIN": settings.APP_DOMAIN,
         "DATA_THEME": "light",
         "GOOGLE_OAUTH2_CLIENT_ID": settings.GOOGLE_OAUTH2_CLIENT_ID,
