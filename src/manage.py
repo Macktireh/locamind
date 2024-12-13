1fa3335166c0c9e6247061890dd98795d8a44a17
Macktireh <abdimack97@gmail.com>
2024-12-13 22:24:15 +0100

    refactoring of the configuration code

--- src/manage.py
+++ src/manage.py
@@ -8,11 +8,6 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
-    print()
-    print("DJANGO SETTINGS MODULE:", os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local"))
-    print("DJANGO SETTINGS MODULE:", os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local"))
-    print("DJANGO SETTINGS MODULE:", os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local"))
-    print()
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
